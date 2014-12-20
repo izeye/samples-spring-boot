b83daa26a3d8a635e136ad7008328fc1b1a673b7
izeye <izeye@naver.com>
2014-12-21 02:06:33 +0900

    Add some application listener tests.

--- src/main/java/samples/springboot/Application.java
+++ src/main/java/samples/springboot/Application.java
@@ -3,6 +3,10 @@ package samples.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.event.SpringApplicationEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +15,29 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
+
+	@Bean
+	public ApplicationListener<ApplicationEvent> applicationListener() {
+		return new ApplicationListener<ApplicationEvent>() {
+			@Override
+			public void onApplicationEvent(ApplicationEvent event) {
+				System.out.println("In applicationListener: " + event);
+			}
+		};
+	}
+
+	// NOTE:
+	// Impossible to register ApplicationListener for SpringApplicationEvents
+	// by bean.
+	@Bean
+	public ApplicationListener<SpringApplicationEvent> springApplicationListener() {
+		return new ApplicationListener<SpringApplicationEvent>() {
+			@Override
+			public void onApplicationEvent(SpringApplicationEvent event) {
+				System.out.println("In springApplicationListener: " + event);
+			}
+		};
+	}
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
