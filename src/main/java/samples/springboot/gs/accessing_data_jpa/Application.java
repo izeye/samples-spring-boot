b76593ac449162aa0cd41049686e94be68b67816
izeye <izeye@naver.com>
2014-12-24 13:27:20 +0900

    Remove an unused import.

--- src/main/java/samples/springboot/gs/accessing_data_jpa/Application.java
+++ src/main/java/samples/springboot/gs/accessing_data_jpa/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
