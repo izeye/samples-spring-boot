34ab1e0aee6eadcc867ddf1cd5afe43beca5dfe7
izeye <izeye@naver.com>
2015-01-26 21:13:56 +0900

    Add a custom Tomcat RemoteAddrValve.

--- src/main/java/samples/springboot/gs/serving_web_content/TestController.java
+++ src/main/java/samples/springboot/gs/serving_web_content/TestController.java
@@ -1,10 +1,11 @@
 package samples.springboot.gs.serving_web_content;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by izeye on 15. 1. 22..
@@ -20,6 +21,11 @@ public class TestController {
 	@RequestMapping("/gs/serving-web-content/testHeaders")
 	public String testHeaders(@RequestHeader("name") String name) {
 		return name;
+	}
+
+	@RequestMapping("/gs/serving-web-content/testRemoteAddr")
+	public String testRemoteAddr(HttpServletRequest request) {
+		return request.getRemoteAddr();
 	}
 
 }
---
+++ src/main/java/samples/springboot/gs/serving_web_content/TomcatRemoteAddrValveCustomizer.java
@@ -0,0 +1,25 @@
+package samples.springboot.gs.serving_web_content;
+
+import org.apache.catalina.valves.RemoteAddrValve;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by izeye on 15. 1. 26..
+ */
+@Component
+public class TomcatRemoteAddrValveCustomizer implements EmbeddedServletContainerCustomizer {
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		TomcatEmbeddedServletContainerFactory tomcatContainer =
+				(TomcatEmbeddedServletContainerFactory) container;
+		RemoteAddrValve remoteAddrValve = new RemoteAddrValve();
+//		remoteAddrValve.setAllow("127.0.0.1");
+		remoteAddrValve.setAllow("0:0:0:0:0:0:0:1");
+		tomcatContainer.addContextValves(remoteAddrValve);
+	}
+
+}
